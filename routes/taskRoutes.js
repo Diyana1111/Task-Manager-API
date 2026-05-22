db21e78c6a843d6729600822b96c87286b21073c
Diyana1111 <diyanasenadi1111@gmail.com>
2026-05-22 17:40:13 +0530

    Setup SQLite database and task routes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import db from "./database/db.js";
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+    const { title, description } = req.body;
+
+    const query = `
+        INSERT INTO tasks (title, description)
+        VALUES (?, ?)
+    `;
+
+    db.run(query, [title, description], function (err) {
+        if (err) {
+            return res.status(500).json({
+                error: err.message,
+            });
+        }
+
+        res.status(201).json({
+            id: this.lastID,
+            title,
+            description,
+        });
+    });
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-import express, { json } from "express";
+import express from "express";
+import db from "./database/db.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 
-app.use(json());
+app.use(express.json());
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        completed INTEGER DEFAULT 0
+    )
+`);
 
 app.get("/", (req, res) => {
     res.send("API Running");
 });
+
+app.use("/api/tasks", taskRoutes);
 
 const PORT = 5000;
 
